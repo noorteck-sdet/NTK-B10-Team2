f18d196fdb4629e81d00aebea72ed94dccf0d314
samiahmadi <samiahmadi@10.0.0.189>
2023-07-30 12:14:22 -0400

    Personal Information

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -5,4 +5,14 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		System.out.println("Demo..");
 	}
+	
+	public void PersonalInfo () {
+		
+		String name = "Sami";
+		String Position = "Project Manager";
+		int age = 39;
+		String Address = "Alexandria, VA, USA";
+		
+		System.out.println(name + Position + Address);
+	}
 }
