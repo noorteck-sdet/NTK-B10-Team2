e9076a4f2dd3c41201ea65434dda3ce4f4c2936b
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-30 12:37:22 -0400

    Merge branch 'master' of https://github.com/noorteck-sdet/NTK-B10-Team2.git

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -6,27 +6,24 @@ public class DemoDay3 {
 		System.out.println("Demo..");
 	}
 
-	public void GetInfo () {
-			System.out.println("Print the personal information");
-System.out.println("zainab");
-}
-
-
-<<<<<<< HEAD
-
-	public static void GetWeatherInfo () {
-		System.out.println("Today is hot and sunny");
-}
-	public static void WeatherInfo () {
-		System.out.println("Today is sunny");
-		
+	public void GetInfo() {
+		System.out.println("Print the personal information");
+		System.out.println("zainab");
 	}
-		public void sport () {
-			
-			System.out.println("Soccer");
-		
-}
-=======
-//importance of following stash process && communicating... :)
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/NTK-B10-Team2.git
+
+	public static void GetWeatherInfo() {
+		System.out.println("Today is hot and sunny");
+	}
+
+	public static void WeatherInfo() {
+		System.out.println("Today is sunny");
+
+	}
+
+	public void sport() {
+
+		System.out.println("Soccer");
+		System.out.println("test");
+	}
+
 }
