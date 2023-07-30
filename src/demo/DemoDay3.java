f7d03377871c14a3e0669d0ec29577aa73efedfd
AbdulSamiAhmadi <140773647+AbdulSamiAhmadi@users.noreply.github.com>
2023-07-30 12:12:04 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -5,4 +5,8 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		System.out.println("Demo..");
 	}
+
+	public void GetInfo () {
+			System.out.println("Print the personal information");
+
 }
